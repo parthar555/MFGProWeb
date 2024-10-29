e288c107ff7e14c1325627c2919ff64814f462d8
parthar555 <partharoysoft@gmail.com>
2024-10-29 14:08:01 -0500

    changed font to white

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,14 @@ import mySvg from './assets/logo.svg';
 import BootStrapMenu from "./Components/bootMenu"
 import DJButtonWithPopover from './Components/logoutBtn';
 import { GrCluster, GrDatabase, GrCompare, GrShield, GrInbox, GrCompliance } from "react-icons/gr";
+import { icons } from 'antd/es/image/PreviewGroup';
 
 const App = () => {
   const contentBoxes = [
-    { title: "Forms Builder", color: "#6e6c9a", fontColor: "#333333", icon: <GrDatabase />,content:"Create forms for operators to work with and report issues in real time" },
-    { title: "Shift Planner", color: "#d8bcbd", fontColor: "#333333", icon: <GrCluster /> ,content:"Plan and assign operators for a shift"},
-    { title: "Shift Leader Planner", color: "#a37f7e", fontColor: "#333333", icon: <GrCompare />,content:"Plan the shifts assigning production lines to supervise" },
-    { title: "Workflows Manager", color: "#6e6c9a", fontColor: "#333333", icon: <GrCompliance /> ,content:"Build the workflows that staff will perform during their shift"},
+    { title: "Forms Builder", color: "#6e6c9a", fontColor: "white", icon: <GrDatabase />,content:"Create forms for operators to work with and report issues in real time" },
+    { title: "Shift Planner", color: "#d8bcbd", fontColor: "white", icon: <GrCluster /> ,content:"Plan and assign operators for a shift"},
+    { title: "Shift Leader Planner", color: "#a37f7e", fontColor: "white", icon: <GrCompare />,content:"Plan the shifts assigning production lines to supervise" },
+    { title: "Workflows Manager", color: "#6e6c9a", fontColor: "white", icon: <GrCompliance /> ,content:"Build the workflows that staff will perform during their shift"},
     { title: "Digital Quality", color: "#504a7a", fontColor: "white", icon: <GrInbox />,content :"Tracks quality in digital forms" },
     { title: "Holds", color: "#2c2039", fontColor: "white", icon: <GrShield />,content : "Investigate and complete holds process" },
 
